d1d68d9ef65015c339f6153ffbe4e0bd8cfba3bf
Nicolas Lindström <nicolas.lindstroem@protonmail.com>
2019-11-27 23:11:53 +0100

    2d check and solve

--- sudoku.py
+++ sudoku.py
@@ -23,21 +23,20 @@ def print_grid(grid):
             print(border)
 
 
-def check(g, index):
-    cur_digit = g[index]
-    cur_col, cur_row = index % 9, index // 9
+def check(g, col, row):
+    cur_digit = g[row][col]
 
     # Check in row
-    if g[cur_row*9:(cur_row+1)*9].count(cur_digit) > 1:
+    if g[row].count(cur_digit) > 1:
         return False
 
     # Check in column
-    if g[cur_col:cur_col+81:9].count(cur_digit) > 1:
+    if [r[col] for r in g].count(cur_digit) > 1:
         return False
 
     # Check in square
-    sq_col, sq_row = 3 * (cur_col // 3), 3 * (cur_row // 3)
-    cur_sq = sum([g[r*9+sq_col:r*9+sq_col+3] for r in range(sq_row, sq_row + 3)], [])
+    sq_col, sq_row = 3 * (col // 3), 3 * (row // 3)
+    cur_sq = sum([r[sq_col:sq_col+3] for r in g[sq_row:sq_row+3]], [])
     if cur_sq.count(cur_digit) > 1:
         return False
 
@@ -45,23 +44,26 @@ def check(g, index):
 
 
 def solve(g):
-    empty_positions = [i for i, _ in filter(lambda d: d[1] == 0, enumerate(g))]
+    empty_positions = []
+    for i, r in enumerate(g):
+        empty_positions += [(j, i) for j, _ in filter(lambda d: d[1] == 0, enumerate(r))]
+
     cur_square_pointer = 0
 
     # Step through empty squares
     while 0 <= cur_square_pointer < len(empty_positions):
 
         # Increment current square
-        cur_square = empty_positions[cur_square_pointer]
-        g[cur_square] += 1
+        col, row = empty_positions[cur_square_pointer]
+        g[row][col] += 1
 
         # If 1-9 checked, reset and move back
-        if g[cur_square] > 9:
-            g[cur_square] = 0
+        if g[row][col] > 9:
+            g[row][col] = 0
             cur_square_pointer -= 1
 
         # Else move forward if current number checks out
-        elif check(g, cur_square):
+        elif check(g, col, row):
             cur_square_pointer += 1
 
 
